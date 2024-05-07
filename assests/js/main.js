dc313cf43be8ad1cf3f0d8f0815adc69a0858513
AliNazarovv <alirn@code.edu.az>
2024-05-07 17:23:57 +0400

    Contact hissesi

---
+++ assests/js/main.js
@@ -0,0 +1,9 @@
+// let now = new Date();
+// console.log(now);
+// console.log(now.getDate());
+// console.log(now.getHours());
+// console.log(now.getMonth());
+// console.log(now.getFullYear());
+// console.log(now.getMinutes());
+
+
